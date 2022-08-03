393016b30ed33b0f223a1a6d5a5c22949425c393
Kemail-terminal <sulkemref@gmail.com>
2022-08-02 20:22:33 -0500

    Day02_EscapeSequence

---
+++ src/day02_escapeSequences/EscapeSequences.java
@@ -0,0 +1,34 @@
+package day02_escapeSequences;
+
+public class EscapeSequences {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello students\nToday we have Java class");
+
+        System.out.println("-----------------------------------------");
+
+        System.out.println("\tJava Programming Language");
+        System.out.println("\t\tJave Developer Course Batch 1");
+
+        System.out.println("-----------------------------------------");
+
+        System.out.println("\\");//    \\
+
+        System.out.println("-----------------------------------------");
+
+        System.out.println("My favorite TV show is \"The Walking Dead\"");//    \"
+
+    }
+
+
+}
+
+
+/*
+    1. \n: New Line
+    2. \t: paragraph space
+    3. \\: \ - back slash
+    4. \": " - double quote
+
+ */
