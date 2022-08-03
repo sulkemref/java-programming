af446f9948f0f50cee1d7483eb4f5055a222ea45
Kemail-terminal <sulkemref@gmail.com>
2022-08-02 20:21:52 -0500

    Day02_About

---
+++ src/day02_escapeSequences/AboutMe.java
@@ -0,0 +1,26 @@
+package day02_escapeSequences;
+
+public class AboutMe {
+
+
+    public static void main(String[] args) {
+
+        System.out.print("\tMy favorite book is\"`How to fix\"\n\tMy favorite quote is \"Let it be\"\n" +
+                "\tMy favorite TV show is \"American dad\"\n\tMy favorite music is POP music");
+
+
+
+
+    }
+}
+
+
+
+/*
+1. Create a class named AboutMe.java
+2. Write a code to print the following info about you:
+1. Your favorite book
+2. Your favorite quote
+3. Your favorite TV show
+4. Your favorite music
+ */
