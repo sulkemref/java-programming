80dbe3cf6c35076de1c63570e2b2622b8cf727a1
Kemail-terminal <sulkemref@gmail.com>
2022-08-02 20:22:50 -0500

    Day02_Print_vs_println

---
+++ src/day02_escapeSequences/Println_vs_Print.java
@@ -0,0 +1,10 @@
+package day02_escapeSequences;
+
+public class Println_vs_Print {
+    public static void main(String[] args) {
+
+        System.out.print("Hello everyone");
+        System.out.print("How are you today?");
+
+    }
+}
