9eb539fc1ff24b61bf9ff0e8fe612eac4bf58c6a
Kemail-terminal <sulkemref@gmail.com>
2022-08-02 20:22:06 -0500

    Day02_Employee_Info

---
+++ src/day02_escapeSequences/EmployeeInfo.java
@@ -0,0 +1,12 @@
+package day02_escapeSequences;
+
+public class EmployeeInfo {
+    public static void main(String[] args) {
+        System.out.println("Name: Kemal");
+        System.out.println("Gender: Male");
+        System.out.println("Age: 32");
+        System.out.println("Job Title: Jave Developer");
+        System.out.println("Company: Apple Inc");
+        System.out.println("Salary: $95000");
+    }
+}
