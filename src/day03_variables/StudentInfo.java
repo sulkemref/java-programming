ef6346143db64d1027eadb1e353f1eb332c18cd9
Kemail-terminal <sulkemref@gmail.com>
2022-08-08 20:25:22 -0500

    Day03_Student_info

---
+++ src/day03_variables/StudentInfo.java
@@ -0,0 +1,49 @@
+package day03_variables;
+
+public class StudentInfo {
+
+
+    public static void main(String[] args) {
+
+    //int num1=1,num2=2,num3=3;
+
+        String students_name = "Kemal";
+        int age = 32;
+        char gender = 'M';
+        String  school_name = "Cydeo",
+                student_id = "A19",
+                grade_level = "Mid Level JD";
+        double gpa = 3.8;
+
+        boolean isFulltime = true;
+
+        System.out.println("students_name = " + students_name);
+        System.out.println("age = " + age);
+        System.out.println("gender = " + gender);
+        System.out.println("school_name = " + school_name);
+        System.out.println("grade_level = " + grade_level);
+        System.out.println("student_id = " + student_id);
+        System.out.println("gpa = " + gpa);
+        System.out.println("isFulltime = " + isFulltime);
+
+
+
+
+
+
+    }
+}
+
+/*
+Create a class named StudentInfo.java
+Declare the following variables with appropriate data types:
+    1. students_name
+    2. age
+    3. gender
+    4. school_name
+    5. student_id
+    6. grade_level
+    7. gpa
+
+
+ */
