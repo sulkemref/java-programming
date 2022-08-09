5f64c396a2ffccb8e64315f6862822be13b600ff
Kemail-terminal <sulkemref@gmail.com>
2022-08-08 20:25:07 -0500

    Day03_Replace_variable_name

---
+++ src/day03_variables/ReplaceVatiableName.java
@@ -0,0 +1,20 @@
+package day03_variables;
+
+public class ReplaceVatiableName {
+
+    public static void main(String[] args) {
+
+
+        String student_name = "Josh";
+
+        System.out.println(student_name);
+        System.out.println(student_name);
+        System.out.println(student_name);
+        System.out.println(student_name);
+        System.out.println(student_name);
+        System.out.println(student_name);
+
+
+
+    }
+}
