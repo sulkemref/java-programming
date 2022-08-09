05235daa940df8d8ea31a115ce69e700d495160a
Kemail-terminal <sulkemref@gmail.com>
2022-08-08 20:24:31 -0500

    Day03_Escape_practice

---
+++ src/day03_variables/EscapeSequencePractice.java
@@ -0,0 +1,26 @@
+package day03_variables;
+
+public class EscapeSequencePractice {
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("My Favorite programming language is \"Java\"\n\tJava is an \"Object-Oriented\" programming language");
+
+
+    }
+
+}
+
+/*
+Create a Java file named EscapeSequencesPractice.java
+			1.1 Write a program that can display the following message:
+
+			Output:
+				 	My Favorite programming language is "Java"
+				 		Java is an "Object-Oriented" programming language.
+
+	 		Note: do not use more than one print statement
+
+ */
