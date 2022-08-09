f3c924bf9dee654f97a142b752a6698ff56c920e
Kemail-terminal <sulkemref@gmail.com>
2022-08-08 20:25:42 -0500

    Day03_Variable_Name_rules

---
+++ src/day03_variables/VariableNamingRules.java
@@ -0,0 +1,45 @@
+package day03_variables;
+
+public class VariableNamingRules {
+
+    public static void main(String[] args) {
+
+        // age of the student is 28
+        // salary is 10000
+
+        int age = 28;
+        int salary = 10000;
+
+        //int age = 32; // Variable name must be uniq
+
+        int phoneNumber = 123456789; // camel case
+
+        double salaryBeforeTax = 100000.5;
+
+
+        double salary_after_tax$ = 400000.90;
+
+
+        System.out.println("---------------------------");
+
+        int number = 10;
+        int number1 = 20;
+        int number2 = 30;
+
+
+        //double abstract = 1000; //variable name can not use reserved word
+
+        System.out.println("---------------------------");
+
+        //gender is male
+
+        String gender = "Male";
+        String fullName = "Cydeo School";
+
+
+        System.out.println(gender);
+        System.out.println(fullName);
+
+
+    }
+}
