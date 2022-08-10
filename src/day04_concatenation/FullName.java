1decf662c6f2c3daffcdf24e8ff036044d398f37
Kemail-terminal <sulkemref@gmail.com>
2022-08-09 20:28:51 -0500

    Day04_Full_Name

---
+++ src/day04_concatenation/FullName.java
@@ -0,0 +1,25 @@
+package day04_concatenation;
+
+public class FullName {
+
+    public static void main(String[] args) {
+
+        String firstName = "Daniel",
+                lastName = "Jefferson",
+                fullName = firstName + " " + lastName;
+
+        System.out.println("Your full name is "+ fullName);
+    }
+}
+
+/*
+
+ Create a class named FullName.java
+2. Declare the following variables:
+1. firstName
+2. lastName
+3. Use concatenation to print the full name
+Ex:
+Your full name is Daniel Jefferson
+
+ */
