84c00179a0eee859e1cb06c6abb08d92a6eab39b
Kemail-terminal <sulkemref@gmail.com>
2022-08-09 20:27:16 -0500

    Day04_Concatination_Intro

---
+++ src/day04_concatenation/ConcatinationIntro.java
@@ -0,0 +1,25 @@
+package day04_concatenation;
+
+public class ConcatinationIntro {
+
+    public static void main(String[] args) {
+
+        String name = "Alexander";
+
+        int age = 35;
+
+        int salary = 85_000;
+
+        System.out.println("Hello " + name + ".");
+
+        System.out.println("You are " + age + " years old.");
+
+        System.out.println("Your salary is " + salary+" USD");
+
+
+        int number = 100_000_000;
+
+        System.out.println("number = " + number);
+    }
+
+}
