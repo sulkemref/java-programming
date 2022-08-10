49cc100a2c8dd497d999358ad5cc6d3c9a0d3e76
Kemail-terminal <sulkemref@gmail.com>
2022-08-09 20:27:00 -0500

    Day04_Concatination_vs_Addition

---
+++ src/day04_concatenation/Concatination_vs_Addition.java
@@ -0,0 +1,24 @@
+package day04_concatenation;
+
+public class Concatination_vs_Addition {
+
+    public static void main(String[] args) {
+
+        System.out.println("32" + 1); // "312"
+
+        System.out.println(7 + "1"); // "71"
+
+        System.out.println("13" + "2"); // "132"
+
+        System.out.println("-----------------------------------------------");
+
+        System.out.println(12 + 2.5); //14.5
+
+        System.out.println("-----------------------------------------------");
+
+        System.out.println("The value is " + true);
+
+    }
+}
+
+
