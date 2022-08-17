57a6d8d39ba5f099bdcfcd848287c93e4a2751ee
Kemail-terminal <sulkemref@gmail.com>
2022-08-16 20:54:45 -0500

    Day06_Maximum_Number

---
+++ src/day06_ifStatemets/MaximumNumber.java
@@ -0,0 +1,42 @@
+package day06_ifStatemets;
+
+public class MaximumNumber {
+
+    public static void main(String[] args) {
+
+        int num1=70,
+                num2=70;
+
+        if(num1>num2){
+
+            System.out.println(num1+" is maximum number");
+
+        }
+
+        if(num2>num1){
+
+            System.out.println(num2+" is maximum number");
+        }
+
+        if(num1==num2){
+
+            System.out.println("Equal");
+        }
+
+    }
+}
+
+/*
+
+1. Create a class named MaximumNumber.java
+2. Declare the following variables:
+1. num1
+2. num2
+3. Write a program that can print the maximum number between the
+two numbers above, if both are equal then print Equal
+Ex:
+num1 = 10
+num2 = 20
+output:
+20 is the maximum number
+ */
