e303405974c0e06db6e4ca2a06488f6d9fc8bfea
Kemail-terminal <sulkemref@gmail.com>
2022-08-16 20:57:32 -0500

    Day06_Operator_Precedence

---
+++ src/day06_ifStatemets/OperatorPrecedence.java
@@ -0,0 +1,36 @@
+package day06_ifStatemets;
+
+public class OperatorPrecedence {
+
+    public static void main(String[] args) {
+
+        System.out.println(5+2*3);
+        //                  5+6
+
+        System.out.println( 7+ 3 -2 / 2);
+        //                  7 + 3 - 1 ==>9
+
+        System.out.println(-5 + 3);
+        //                      -2
+
+        System.out.println(10 >9 == 8<7 && "Jave" == "Python" || 'a'=='A');
+        //                  true == false &&
+        //                          false && false || false
+
+
+        System.out.println( 100>=20 && 40*2>60);
+        //                  100>=20&& 80>60
+        //                  true && true
+
+
+        int a = 20;
+
+        a+=10+2*3;
+        // a += 10+6
+        // a+=16
+
+
+    }
+}
+
+
