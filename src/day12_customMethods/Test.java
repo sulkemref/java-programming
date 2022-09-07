b9e08b6580fedb48a9d072bc18704194bdd0e03c
Kemail-terminal <sulkemref@gmail.com>
2022-09-06 21:37:17 -0500

    Day12_Test

---
+++ src/day12_customMethods/Test.java
@@ -0,0 +1,18 @@
+package day12_customMethods;
+
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        int number = 300;
+
+        CustomMethodsWithParameter.oddOrEven(number);
+
+        String firstName ="Vasyl",
+                lastName="Dobrianski";
+
+        CustomMethodsPractice.initial(firstName,lastName);
+
+    }
+}
