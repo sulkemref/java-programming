f00a849b8898738909467c0e96fc134743cfc063
Kemail-terminal <sulkemref@gmail.com>
2022-09-13 21:43:35 -0500

    Day14_For_Loop

---
+++ src/day14_forLoop/ForLoopIntro.java
@@ -0,0 +1,29 @@
+package day14_forLoop;
+
+public class ForLoopIntro {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+
+        System.out.println("-------------------------------------------------------");
+
+        for(int i = 1;i<11;i++){
+
+
+            System.out.println("Hello wold "+i);
+
+        }
+
+
+
+    }
+}
