f88a49868cc34e3340ce18cbd64ead3d2e5ebbcf
Kemail-terminal <sulkemref@gmail.com>
2022-09-20 22:06:06 -0500

    Day16_NestedLoopPractice

---
+++ src/day16_nestedLoop/NestedLoopPractice.java
@@ -0,0 +1,30 @@
+package day16_nestedLoop;
+
+public class NestedLoopPractice {
+
+
+    public static void main(String[] args) {
+
+        String str = "aaabbcccccddeeeeeff";
+        String result = "";
+
+        for (int j = 0; j < str.length(); j++) {
+
+            char ch = str.charAt(j); // each character of str
+            int count = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if(str.charAt(i)==ch) {
+                    count++;
+                }
+            }
+
+            if(count == 2 && !result.contains(ch+"")){
+                result +=ch ;
+
+            }
+        }
+        System.out.println(result);
+
+
+    }
+}
