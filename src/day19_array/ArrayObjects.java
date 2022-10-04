73242c1774d97b9df0a7798abd44db5d53189e3b
Kemail-terminal <sulkemref@gmail.com>
2022-10-03 22:18:59 -0500

    Day19_Array_Objects

---
+++ src/day19_array/ArrayObjects.java
@@ -0,0 +1,15 @@
+package day19_array;
+
+import java.util.Arrays;
+
+public class ArrayObjects {
+
+    public static void main(String[] args) {
+
+        String [] a1 = {"Umran","Mehmet"};
+
+        a1 = new String[]{"James", "Daniel"};
+
+        System.out.println(Arrays.toString(a1));
+    }
+}
