5474847d303c5cb63c6e1a54d5f1a2a68327322f
Kemail-terminal <sulkemref@gmail.com>
2022-10-10 22:29:36 -0500

    Day21_Array_Contains

---
+++ src/day21_multiDimensionalArray/ArrayContains.java
@@ -0,0 +1,13 @@
+package day21_multiDimensionalArray;
+
+import utilities.ArraysUtility;
+
+public class ArrayContains {
+
+    public static void main(String[] args) {
+
+        String [] array = {"Hello","World","My","Home"};
+
+        System.out.println(ArraysUtility.contains(array,"World"));
+    }
+}
