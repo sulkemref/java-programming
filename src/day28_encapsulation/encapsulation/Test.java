d20b2a48b6ddeb6c5bec9b2b8b34782276d66faf
Kemail-terminal <sulkemref@gmail.com>
2022-11-02 23:26:31 -0500

    Day28_Encapsulation_Test

---
+++ src/day28_encapsulation/encapsulation/Test.java
@@ -0,0 +1,23 @@
+package day28_encapsulation.encapsulation;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        Student student = new Student();
+
+       student.setAge(25);
+
+        System.out.println(student.getAge());
+
+
+        //student.setName("Jonn");
+
+        System.out.println(student.getName());
+
+
+
+
+    }
+}
