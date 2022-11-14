3b17ef43ace64ba75ac9288e6264ac9241478c77
Kemail-terminal <sulkemref@gmail.com>
2022-11-14 10:47:02 -0500

    Day31_protected_access_modifier

---
+++ src/day31_inheritance/ProtectedAccessModifier.java
@@ -0,0 +1,10 @@
+package day31_inheritance;
+
+public class ProtectedAccessModifier {
+
+    protected static int a = 100;
+
+    protected static void methodA(){
+        System.out.println("");
+    }
+}
