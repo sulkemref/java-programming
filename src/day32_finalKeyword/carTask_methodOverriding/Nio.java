5cab83b93327e1822f50eba3b7fb5f26ccd8be64
Kemail-terminal <sulkemref@gmail.com>
2022-11-15 10:49:24 -0500

    Day32_carTask_Nio

---
+++ src/day32_finalKeyword/carTask_methodOverriding/Nio.java
@@ -0,0 +1,12 @@
+package day32_finalKeyword.carTask_methodOverriding;
+
+public class Nio extends Tesla{
+
+    public Nio(String model, String color, int year, double price) {
+        super(model, color, year, price);
+    }
+
+
+
+}
+
