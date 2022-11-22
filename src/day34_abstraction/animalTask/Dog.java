d59eb51c596e337bc3ac26aeb0ea34650af67218
Kemail-terminal <sulkemref@gmail.com>
2022-11-22 10:55:59 -0500

    Day34_animalTask_Dog

---
+++ src/day34_abstraction/animalTask/Dog.java
@@ -0,0 +1,22 @@
+package day34_abstraction.animalTask;
+
+public class Dog extends Animal implements Playable{
+
+    public Dog(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getClass().getSimpleName()+" "+ getName()+" is eating dog food");
+    }
+
+    @Override
+    public void play() {
+
+    }
+
+    public void bark(){
+        System.out.println(getName()+ " is barking");
+    }
+}
