71c7d399b74fa57778526f18d10292dc92c7d3c9
Kemail-terminal <sulkemref@gmail.com>
2022-11-22 10:56:17 -0500

    Day34_animalTask_Duck

---
+++ src/day34_abstraction/animalTask/Duck.java
@@ -0,0 +1,28 @@
+package day34_abstraction.animalTask;
+
+public class Duck extends Animal implements Playable,Flyable,Swimmable{
+
+    public Duck(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getClass().getSimpleName()+" "+getName()+ " is eating worms");
+    }
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void swim() {
+
+    }
+}
