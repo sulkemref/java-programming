d3ded7285ddaf31a71f347371c93e77337dd6ecf
Kemail-terminal <sulkemref@gmail.com>
2022-11-29 11:03:04 -0500

    Day35_transportTask_AutoPark

---
+++ src/day35_polymorphism/transpotationTask/AutoPark.java
@@ -0,0 +1,17 @@
+package day35_polymorphism.transpotationTask;
+
+public interface AutoPark {
+
+    boolean hasAutoPark=true;
+
+    void autoPark();
+
+
+
+}
+/*
+2. Create an interface named AutoPark
+
+				abstract methods:
+					autoPark()
+ */
