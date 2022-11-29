371725fd204bd14a2dd44da472fc67d39b0fe47d
Kemail-terminal <sulkemref@gmail.com>
2022-11-29 11:03:14 -0500

    Day35_transportTask_AutoPilot

---
+++ src/day35_polymorphism/transpotationTask/AutoPilot.java
@@ -0,0 +1,17 @@
+package day35_polymorphism.transpotationTask;
+
+public interface AutoPilot extends AutoPark{
+
+    boolean hasAutoPilot = true;
+
+    void selfDrive();
+
+
+
+}
+/*
+3. Create a sub interface of AutoPark named AutoPilot:
+
+				abstract methods:
+						autoPilot()
+ */
