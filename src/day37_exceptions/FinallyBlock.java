d13e74306f662e1163bd6cfc40809b79fbb59b36
Kemail-terminal <sulkemref@gmail.com>
2022-12-05 11:08:00 -0500

    Day37_Finally_Block

---
+++ src/day37_exceptions/FinallyBlock.java
@@ -0,0 +1,27 @@
+package day37_exceptions;
+
+public class FinallyBlock {
+
+    public static void main(String[] args) {
+
+
+        try{
+
+            System.out.println(9/0);
+
+        }catch (NullPointerException e){
+
+            System.out.println("Runtime Exception has been caught");
+            e.printStackTrace();
+           // System.exit(1);
+
+        }finally{
+
+            System.out.println("Finally Block");
+
+        }
+
+
+
+    }
+}
