7aaa038a970fd479da76fee4097665b98505f5d7
Kemail-terminal <sulkemref@gmail.com>
2022-12-06 11:29:49 -0500

    Day38_TestPersonObject

---
+++ src/day38_exceptions/TestPersonObject.java
@@ -0,0 +1,22 @@
+package day38_exceptions;
+
+public class TestPersonObject {
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Daniel", 90,'M');
+
+        System.out.println(person1);
+
+        /*
+        try {
+            person1.setAge(-25);
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+*/
+
+        System.out.println(person1);
+
+    }
+}
