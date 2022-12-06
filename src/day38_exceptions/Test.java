445947238abfaf51ecf8e3c7ca7dc6068a1572a8
Kemail-terminal <sulkemref@gmail.com>
2022-12-06 11:30:48 -0500

    Day38_Test

---
+++ src/day38_exceptions/Test.java
@@ -0,0 +1,28 @@
+package day38_exceptions;
+
+import utilities.Library;
+
+import java.time.LocalTime;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello Cydeo");
+
+        //Thread.sleep(3500);
+        Library.sleep(3.5);
+
+        System.out.println("How are you today?");
+
+        if(LocalTime.now().equals(LocalTime.of(4,0))){
+            throw new BreakTimeException();
+        }
+
+        //throw new RuntimeException("Something went wrong");
+        throw new BreakTimeException("It's time to go home");
+
+       // throw new BreakTimeException();
+    }
+}
